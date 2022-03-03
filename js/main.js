b4996b7527ad672efddda773ae9bb4363085183e
oott0228 <oott0228@gmail.com>
2022-03-03 18:55:30 +0900

    リプレイ機能追加

--- js/main.js
+++ js/main.js
@@ -3,13 +3,15 @@
 {
     function setWord() {
         countDown();
-        word = words.splice(Math.floor(Math.random() * words.length), 1)[0];
+        word = words[index];
+        index++;
         target.textContent = word;
         loc = 0;
     }
 
     function countDown() {
-        limitTime = 6;
+        limitTime = 10;
+        limit.style.visibility = 'visible'; // 2回目最初変な数字が表示されてしまう。3回目以降カウントダウンが表示されない。
         if(loc !== 0) { return; } 
 
         const countDown = () => {
@@ -20,10 +22,14 @@
         const intervalId = setInterval(() => {
         countDown();
 
-        if (limitTime < 2) {
+        if (limitTime < 1) {
             clearInterval(intervalId);
             target.textContent = 'Game Over!';
-            target.style.backgroundColor = 'red'
+            target.style.backgroundColor = 'red';
+
+            replay.textContent = 'Click to Replay!';
+            replay.style.visibility = 'visible';
+
         }}, 1000);
     }
     
@@ -36,28 +42,44 @@
         'black',
         'yellow',
         'orange',
-        'green'
+        'green',
     ];
-
+    
     let word;
     let loc = 0;
+    let index = 0;
     let count = 4;
     let startTime; 
     let isPlaying = false;
     let isWrong = false;
     let limitTime;
-    const index = words.length;
     const target = document.getElementById('target');
     const limit = document.getElementById('limit');
+    const replay = document.getElementById('replay');
 
-    document.addEventListener('click', () => {
+    function shuffleWords(words,randWords) {
+        words.sort(() => Math.random() - 0.5);
+    }
+
+    replay.addEventListener('click', () => {
+        result.style.visibility = 'hidden';
+        replay.style.visibility = 'hidden';
+        target.textContent = '';
+        limit.textContent = '';
+        target.style.visibility = 'visible';
+        isWrong = false;
+        isPlaying = false;
+
+        target.style.backgroundColor = 'transparent';
+
         if (isPlaying) { return; }
+        index = 0;
 
+        count = 4;
         const countDown = () => {
             count--;
             target.textContent = count;
         } 
-
         const intervalId = setInterval(() => {
             countDown();
             if(count < 2) {
@@ -66,9 +88,30 @@
 
         isPlaying = true;
         startTime = Date.now() + 4000;
+        shuffleWords(words);
         setTimeout(setWord, 4000);
     });
 
+    target.addEventListener('click', () => {
+        if (isPlaying) { return; }
+        index = 0;
+        
+        const countDown = () => {
+            count--;
+            target.textContent = count;
+        } 
+        const intervalId = setInterval(() => {
+            countDown();
+            if(count < 2) {
+                clearInterval(intervalId);
+            }}, 1000);
+
+        isPlaying = true;
+        startTime = Date.now() + 4000;
+        shuffleWords(words);
+        setTimeout(setWord, 4000);
+    });
+    
     document.addEventListener('keydown', e => {
         if(e.key !== word[loc]) {
             target.style.backgroundColor = 'red';
@@ -76,16 +119,20 @@
         } else {
             target.style.backgroundColor = 'transparent';
         }
-
+        
         loc++;
         target.textContent = '_'.repeat(loc) + word.substring(loc);
-
         if (loc !== word.length) { return; }
 
-        if (words.length === 0) {
+        if (index === words.length) {
+            target.style.visibility = 'hidden';
+            limit.style.visibility = 'hidden';
+            replay.textContent = 'Click to Replay!';
             const elapsedTime = ((Date.now() - startTime) / 1000).toFixed(2);
             const result = document.getElementById('result');
             result.textContent = `Finished! ${elapsedTime} seconds!`;
+            result.style.visibility = 'visible';
+            replay.style.visibility = 'visible';
             return;
         }
         setWord();
